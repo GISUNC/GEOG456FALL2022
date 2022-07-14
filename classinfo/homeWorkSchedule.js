a4c7223b568a7357d00ab29e4f905f8357af61a5
GISUNC <39126373+maparce@users.noreply.github.com>
2022-07-13 21:44:05 -0400

    Update homeWorkSchedule.js

---
+++ classinfo/homeWorkSchedule.js
@@ -0,0 +1,210 @@
+var hw = {
+    "Tue Aug 18 2022": [
+      {
+        "title": "Make basic Upload page to GitHub",
+        "deadline": "before next class",
+        "value": 4,
+        "rubric": {
+          "page runs from Github": 1,
+          "div and h1 elements created and have style": 1,
+          "div has onclick event to console.log": 1,
+          "h1 has onclick event with alert": 1
+        },
+        "description": "Create a webpage that has a div with a background color besides white and an h1 element. The h1 element has to have a onclick event with an alert that says Hello there, and the div should have an onclick event that creates a console.log that says the `the div was clicked`. Upload this HTML page to github and submit the link of the page in Sakai. Note: if the link does not work from your cellphone and works in your computer, it probably means that you sent me a local link and not the webpage link in Github"
+      }
+    ],
+    "Thu Aug 23 2022": [
+      {
+        "title": "Show and Tell 1",
+        "deadline": "before next class",
+        "value": 5,
+        "rubric": {
+          "provided 3 valuable examples": 3,
+          "the examples were put in context of the student`s interest": 1,
+          "presented within the alloted time": 1
+        },
+        "description": "Find three visualizations that you would like to share with the rest of the class. This visualizations should focus on presenting time and not space. Hence, do not present figures that have map components we will share those at another time. The figures should be related to your area of interest"
+      },
+      {
+        "title": "Find dataset",
+        "deadline": "before next class",
+        "value": 1,
+        "rubric": {
+          "found a dataset": 1
+        },
+        "description": "find a dataset that has a temporal component and that is related to your interest. You will use this dataset for future homeworks"
+      }
+    ],
+    "Tue Aug 25 2022": [
+      {
+        "title": "Make several Plotly figures and post in a Page",
+        "deadline": "before 2next class",
+        "value": 12,
+        "rubric": {
+          "bar plot": 1,
+          "scatter plot": 1,
+          "polar plot": 1,
+          "another complex plot ": 1,
+          "used colors that facilitate reading of graph data": 4,
+          "graphs have titles and legend": 4,
+          "described process and limitations for each figure": 4
+        },
+        "description": "Make a scaterplot, a barplot, a polar plot and some complex plot using Plotly and a dataset of your own interest that has a temporal component. Try to create figures that explore the how a certain process changes through time. Each figure could be a single HTML file with a Title and a short description of the limitations and benefits of the type of visualization used. Create an index.html page that has a link to each of the plots and submit the link to the index.html file in Sakai."
+      }
+    ],
+    "Thu Aug 25 2022": [],
+    "Tue Aug 30 2022": [
+      {
+        "title": "Reading discussion questions",
+        "deadline": "before next class",
+        "value": 5,
+        "rubric": {
+          "submitted well-thought questions on time": 3,
+          "active participation in class discussion": 2
+        },
+        "description": "Come up with two or three discussion points related to each article posted in Sakai. These should be questions to provoke reflection and conversation with your classmates: if you have factual questions about anything mentioned in these articles, you should do the research to answer them yourself."
+      }
+    ],
+    "Thu Sep 01 2022": [
+      {
+        "title": "Show and Tell 2",
+        "deadline": "before next class",
+        "value": 5,
+        "rubric": {
+          "provided 3 valuable examples": 3,
+          "the examples were put in context of the student`s interest": 1,
+          "presented within the alloted time": 1
+        },
+        "description": "Find three web based geovisualizations that you would like to share with the rest of the class. This visualizations should focus on presenting both time and space using mostly vectors. The figures should be related to your area of interest"
+      },
+      {
+        "title": "Find dataset",
+        "deadline": "before next class",
+        "value": 1,
+        "rubric": {
+          "found a dataset": 1
+        },
+        "description": "find a dataset that has a both a temporal and geographic component and that is related to your interest. You will use this dataset for a future homework. It can be CSV, geoJSON or a shapefile"
+      }
+    ],
+    "Tue Sep 06 2022": "No Classes",
+    "Thu Sep 08 2022": [
+      {
+        "title": "Make a leaflet map with at least one maker",
+        "deadline": "before next class",
+        "value": 3,
+        "rubric": {
+          "leaflet map with OSM tile background": 1,
+          "has a marker": 1,
+          "the marker has a popup": 1
+        },
+        "description": "Make a leaflet map outside of Chapel Hill. The map has an OSM tile background, a marker and the marker has popup with information about the place that is marking"
+      }
+    ],
+    "Tue Sep 13 2022": [
+      {
+        "title": "Make a leaflet map with a slider or button that controls a time variable",
+        "deadline": "before 2next class",
+        "value": 3,
+        "rubric": {
+          "leaflet map displays data": 1,
+          "the slider or button controls a time variable": 4
+        },
+        "description": "Make a leaflet map using a dataset that has a temporal component. Use a button or a slider to control how features that have a variable related to time"
+      }
+    ],
+    "Thu Sep 15 2022": [],
+    "Tue Sep 20 2022": [
+      {
+        "title": "Reading discussion questions",
+        "deadline": "before next class",
+        "value": 5,
+        "rubric": {
+          "submitted well-thought questions on time": 3,
+          "active participation in class discussion": 2
+        },
+        "description": "Come up with two or three discussion points related to each article posted in Sakai. These should be questions to provoke reflection and conversation with your classmates: if you have factual questions about anything mentioned in these articles, you should do the research to answer them yourself."
+      }
+    ],
+    "Thu Sep 22 2022": [
+      {
+        "title": "Show and Tell 3",
+        "deadline": "before next class",
+        "value": 5,
+        "rubric": {
+          "provided 3 valuable examples": 3,
+          "the examples were put in context of the student`s interest": 1,
+          "presented within the alloted time": 1
+        },
+        "description": "Find three geovisualizations that you would like to share with the rest of the class. This visualizations should focus on presenting both time and space using remote sensing images or static maps that engage with the topic of space and time. The figures should be related to your area of interest"
+      }
+    ],
+    "Tue Sep 27 2022": [
+      {
+        "title": "Make a GEE Map",
+        "deadline": "before next class",
+        "value": 3,
+        "rubric": {
+          "create a public GEE map": 1,
+          "the GEE map has the name of the student": 1,
+          "the layers have a proper name and not a default": 1
+        },
+        "description": "Watch the video on how to create a GEE map and make it public. Make a map with in your account. The map should have a title and the layers should have a proper name and not the default `layer` value"
+      }
+    ],
+    "Thu Sep 29 2022": [
+      {
+        "title": "Make an interactive GEE map",
+        "deadline": "before next2 class",
+        "value": 10,
+        "rubric": {
+          "Interactive component": 3,
+          "Histogram matched at least 2 images of different time": 2,
+          "the layers have a proper name and not a default value": 1,
+          "The student put into context the observed changes": 2,
+          "The presentation was within the alloted time and the student made an effective presentation": 2
+        },
+        "description": "Watch the video on how to create a GEE map and make it public. Make a map with in your account. The map should have a title and the layers should have a proper name and not the default `layer` value"
+      }
+    ],
+    "Tue Oct 04 2022": [],
+    "Thu Oct 06 2022": [],
+    "Tue Oct 11 2022": [
+      {
+        "title": "Reading discussion questions",
+        "deadline": "before next class",
+        "value": 5,
+        "rubric": {
+          "submitted well-thought questions on time": 3,
+          "active participation in class discussion": 2
+        },
+        "description": "Come up with two or three discussion points related to each article posted in Sakai. These should be questions to provoke reflection and conversation with your classmates: if you have factual questions about anything mentioned in these articles, you should do the research to answer them yourself."
+      }
+    ],
+    "Thu Oct 13 2022": [],
+    "Tue Oct 18 2022": [],
+    "Thu Oct 20 2022": "No Classes",
+    "Tue Oct 25 2022": [],
+    "Thu Oct 27 2022": [],
+    "Tue Nov 01 2022": [
+      {
+        "title": "Reading discussion questions",
+        "deadline": "before next class",
+        "value": 5,
+        "rubric": {
+          "submitted well-thought questions on time": 3,
+          "active participation in class discussion": 2
+        },
+        "description": "Come up with two or three discussion points related to each article posted in Sakai. These should be questions to provoke reflection and conversation with your classmates: if you have factual questions about anything mentioned in these articles, you should do the research to answer them yourself."
+      }
+    ],
+    "Thu Nov 03 2022": [],
+    "Tue Nov 08 2022": [],
+    "Thu Nov 10 2022": [],
+    "Tue Nov 15 2022": [],
+    "Thu Nov 17 2022": [],
+    "Tue Nov 22 2022": [],
+    "Thu Nov 24 2022": "No Classes",
+    "Tue Nov 29 2022": [],
+    "Thu Dec 01 2022": []
+  }
